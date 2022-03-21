f65fe5aac326980ae3443ad742176db716551a58
Guicastilh0 <guiluka@hotmail.com>
2022-03-21 10:52:37 -0300

    Seção 5: Angular , interpolação , Diretiva e Bd

--- Front/ProEventos-App/src/app/eventos/eventos.component.ts
+++ Front/ProEventos-App/src/app/eventos/eventos.component.ts
@@ -8,7 +8,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EventosComponent implements OnInit {
 
-  public eventos: any;
+  public eventos: any = [];
+  widthImg: number = 150;
+  marginImg: number =2;
+  ShowImage = true;
+
+  // Filtragem de dados
+  public eventosFiltrados: any = [];
+  private _listFilter: string = '';
+  get listFilter() {
+    return this._listFilter;
+  }
+  set listFilter(value: string) {
+    this._listFilter = value;
+    this.eventosFiltrados = this.listFilter ? this.filtrarEventos(this.listFilter): this.eventosFiltrados;
+  }
+
+  filtrarEventos(filtrarPor: string): any{
+    filtrarPor = filtrarPor.toLocaleLowerCase();
+    return this.eventos.filter(
+      (evento : {tema: string; local: string;}) => evento.tema.toLocaleLowerCase().indexOf(filtrarPor) !== -1 ||
+      evento.local.toLocaleLowerCase().indexOf(filtrarPor) !== -1
+    )
+  }
+
 
   constructor(private http: HttpClient) { }
 
@@ -18,9 +41,15 @@ export class EventosComponent implements OnInit {
 
   public getEventos(): void {
     this.http.get('https://localhost:5001/api/eventos').subscribe(
-      response => this.eventos = response,
+      response => {
+        this.eventos = response
+        this.eventosFiltrados = this.eventos
+      },
       error => console.log(error)
     );
   }
 
+  alterarImagem(){
+    this.ShowImage = !this.ShowImage;
+  }
 }
